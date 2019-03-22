7f5d352c7889bfe13f57ce674a7d9d5765910c97
Ernest Viola <ernest.viol@gmail.com>
2019-03-22 13:58:10 -0700

    Created InitializeDatabase.cs

---
+++ InitializeDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace Library
+{
+    public class InitializeDatabase
+    {
+        public InitializeDatabase()
+        {
+            string connStr = "server=localhost;user=root;port=3306;password=";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                string s0 = "CREATE DATABASE IF NOT EXIST `public_library`";
+                MySqlCommand cmd = new MySqlCommand(s0);
+                cmd.BeginExecuteNonQuery();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
